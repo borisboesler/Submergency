798f1810c39c4d0e36f25fd13bf12ba144bf9382
Boris Boesler <baembel@gmx.de>
2023-03-19 13:18:28 +0100

    Add Empty Project
    Add an empty Xcode project.

---
+++ Submergency/SubmergencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmergencyApp.swift
+//  Submergency
+//
+//  Created by Boris Boesler on 19.03.23.
+//
+
+import SwiftUI
+
+@main
+struct SubmergencyApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
