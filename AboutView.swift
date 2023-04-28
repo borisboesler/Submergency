44bbc16b8a6ef78a90881eedfd540175f4c75595
Boris Boesler <baembel@gmx.de>
2023-04-28 15:32:06 +0200

    feat: Embed Code Version
    Embed the git commit tag as a code status.

--- AboutView.swift
+++ AboutView.swift
@@ -26,17 +26,28 @@ struct AboutView: View {
       Text("Version: \(bundleShortVersion)")
         .font(.title2)
       #if DEBUG
-        Spacer()
-        VStack {
-          Text("DEBUG Build")
-          Text("Bundle version: \(bundleVersion)")
-          Text("git Status: \(bundleGitStatus)")
+        // Note: max number of entries in a View is 10! (see: https://stackoverflow.com/a/65348666)
+        Group {
+          Spacer()
+          VStack {
+            Text("DEBUG Build")
+            Text("Bundle version: \(bundleVersion)")
+          }
+          .padding(5)
+          .background(Color.red)
+          .cornerRadius(10)
         }
-        .padding(5)
-        .background(Color.red)
-        .cornerRadius(10)
       #endif
       Spacer()
+      Group {
+        VStack {
+          Text("Code Status:")
+          Text("\(bundleGitStatus)")
+        }
+        .padding(10)
+        .background(Color.blue)
+        .cornerRadius(10)
+      }
     }
     .padding(5)
   }
